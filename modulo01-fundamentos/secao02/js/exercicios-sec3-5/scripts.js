671a9b86ca483ee8cd209ad56cc90cd5605ebead
Samuel Henrique Bertolini Deotti <samuel_deotti@hotmail.com>
2023-05-02 11:36:27 -0300

    exercicios

---
+++ modulo01-fundamentos/secao02/js/exercicios-sec3-5/scripts.js
@@ -0,0 +1,46 @@
+/*   const name = 'Adriana';
+  const lastName = 'Soares';
+  console.log(`Olá, ${name} ${lastName}!`);
+  function soma(a,b) {
+    let resultado = a + b;
+    return resultado;
+  }
+  let a = 3;
+  let b = 5;
+  console.log(`O resultado da soma de ${a} + ${b} é: ${a+b}`); */
+
+/*   const numeroAleatorio = () => Math.random()
+
+  console.log(numeroAleatorio());
+ */
+
+  /* const hello = nome =>`Olá, ${nome}!`
+  let nome = 'Ivan';
+  console.log(hello(nome)); */
+/* 
+  const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
+  let nome = 'Ivan';
+  let sobrenome = 'Pires';
+  console.log(nomeCompleto(nome, sobrenome)); */
+
+
+
+
+
+/*   // operador ternário, |  (condicao) ? se for afirmativa faz isso : se for falso faz isso
+
+  // pode ser mostrado assim também console.log((idade > 12) && `Voce ja tem mais que 12 anos`)  | so serve para uma condição
+  let speed = 90;  
+  const speedCar = (speed) => (speed >= 120) ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida` 
+    
+  console.log(speedCar(speed)); */
+
+
+/* const ligaMotor = motor => motor = (motor === 'desligado') ? motor = 'ligado' : motor = 'desligado'
+
+console.log(`O seu carro esta: ${ligaMotor('ligado')}`) */
+
+/* const areaCirculo = raio => 3.14 * raio**2
+
+console.log(areaCirculo(5)) */
+
